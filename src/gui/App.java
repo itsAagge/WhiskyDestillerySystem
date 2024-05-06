e7e7de1866acde55b52ffba16a90cef0ca186978
August Juel Aagaard <augustjaagaard@gmail.com>
2024-05-06 11:04:46 +0200

    Added App.java

---
+++ src/gui/App.java
@@ -0,0 +1,12 @@
+package gui;
+
+public class App {
+    public static void main(String[] args) {
+        initContent();
+        //Application.Start(INSERT_FRAME)
+    }
+
+    private static void initContent() {
+
+    }
+}
