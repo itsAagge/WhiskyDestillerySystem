ba5cd803b115e4e51be2e0ec9017b3a94512db8b
drewl <andrewahaz@outlook.dk>
2024-05-16 14:03:14 +0200

    start på LagerDialog

--- src/gui/LagerPane.java
+++ src/gui/LagerPane.java
@@ -5,10 +5,13 @@ import application.model.Fad;
 import application.model.Hylde;
 import application.model.Lager;
 import application.model.Reol;
+import javafx.geometry.HPos;
 import javafx.geometry.Insets;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 
 public class LagerPane extends GridPane {
 
@@ -26,7 +29,36 @@ public class LagerPane extends GridPane {
 
         Label lblAlleLagre = new Label("Alle lagre");
         this.add(lblAlleLagre, 0,0);
-        lvwLager.getItems().setAll();
+        this.add(lvwLager,0,1);
+        lvwLager.getItems().setAll(Controller.getLagre());
+
+        Label lblReol = new Label("Reoler på lageret");
+        this.add(lblReol, 1,0);
+        this.add(lvwReol,1,1);
+
+        Label lblHylde = new Label("Hylder på reolen");
+        this.add(lblHylde,2,0);
+        this.add(lvwHylde,2,1);
+
+        Label lblFad = new Label("Fade på hylden");
+        this.add(lblFad, 3,0);
+        this.add(lvwFad, 3,1);
+
+        Button btnOpret = new Button("Opret");
+        this.add(btnOpret, 0,2);
+        btnOpret.setOnAction(actionEvent -> this.opretAction());
+
+        Button btnFlytFad = new Button("Flyt fad");
+        this.add(btnFlytFad,3,2);
+        this.setHalignment(btnFlytFad, HPos.RIGHT);
+
+
+    }
+
+    private void opretAction() {
+        LagerDialog lagerDialog = new LagerDialog();
+        lagerDialog.showAndWait();
+        lvwLager.getItems().setAll(Controller.getLagre());
 
     }
 
