0b1260955b002c1aa8049641c9db1e98213c1da5
drewl <andrewahaz@outlook.dk>
2024-05-21 12:09:04 +0200

    Rettet PåfyldningTest

--- test/påfyldning/PåfyldningTest.java
+++ test/påfyldning/PåfyldningTest.java
@@ -101,27 +101,6 @@ class PåfyldningTest {
 
     }
 
-    @DisplayName("tilføjDestillatMedMængde: flere mængder end destillater")
-    @Test
-    void tilføjDestillatMedMængde3() {
-        ArrayList<Destillat> destillater = new ArrayList<>();
-        ArrayList<Double> mængder = new ArrayList<>();
-        Korn korn1 = controller.opretKorn("Mark 1", "Byg",LocalDate.of(2023,11,30), 200);
-        Korn korn2 = controller.opretKorn("Mark 2", "Byg",LocalDate.of(2023,12,10), 200);
-        Maltbatch maltbatch1 = controller.opretMaltbatch(200, LocalDate.of(2023, 12, 1), LocalDate.of(2023,12,12), "gær1", korn1);
-        Maltbatch maltbatch2 = controller.opretMaltbatch(200, LocalDate.of(2023, 12, 12), LocalDate.of(2023,12,25), "gær1", korn2);
-        Destillat destillat1 = controller.opretDestillat("1", 200, 80.5, "Snæver", "Tørv", "Smager godt", LocalDate.of(2023,12,24), maltbatch1);
-        Destillat destillat2 = controller.opretDestillat("2", 300, 78.7, "Snævar", null, "Smager dårligt", LocalDate.of(2023, 12, 30), maltbatch2);
-        destillater.add(destillat1);
-        destillater.add(destillat2);
-        mængder.add(100.0);
-        mængder.add(25.0);
-        mængder.add(10.0);
-
-        assertNotEquals(mængder.size(), destillater.size());
-
-    }
-
     @DisplayName("mængdeTilbage TC1: mængdetilbage = 125")
     @Test
     void mængdeTilbage1() {
